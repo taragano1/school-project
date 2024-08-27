57b640af5571844aae838768cfc572123684082b
taragano1 <shirarivkataragano@gmail.com>
2024-08-27 14:35:53 +0300

    tabls update

--- server/dal/tables/users.js
+++ server/dal/tables/users.js
@@ -1,6 +1,6 @@
 const connection = require('../connectToDB')
 
-var sql = "CREATE TABLE users (id VARCHAR(9) PRIMARY KEY, fname VARCHAR(255), lname VARCHAR(255), email VARCHAR(255), phone VARCHAR(255),city VARCHAR(50), birthday VARCHAR(100),address VARCHAR(255), gender_id INT, FOREIGN KEY (gender_id) REFERENCES gender(id))";
+var sql = "CREATE TABLE users (id VARCHAR(9) PRIMARY KEY, fname VARCHAR(255), lname VARCHAR(255), email VARCHAR(255), phone VARCHAR(255),city VARCHAR(50), birthday VARCHAR(100),address VARCHAR(255), gender_id INT,typeOfUser INT, status Boolean ,FOREIGN KEY (gender_id) REFERENCES gender(id))";
 
 //var sql="DROP TABLE users"
 
